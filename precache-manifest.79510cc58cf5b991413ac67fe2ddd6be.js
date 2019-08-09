9ad356de6b22b2f6d429ad69a0bd6ebb4f86bbb7
hwasub Lee <peaceflame9@gmail.com>
2019-08-09 14:52:43 +0900

    Updates

---
+++ precache-manifest.79510cc58cf5b991413ac67fe2ddd6be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398752a598c58c79756ee4b826c410a5",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "04d742598221e74f10a6",
+    "url": "/movie_app_2019/static/css/main.d52dbf69.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/movie_app_2019/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "04d742598221e74f10a6",
+    "url": "/movie_app_2019/static/js/main.42dfa4a2.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
